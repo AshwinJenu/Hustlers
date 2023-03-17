a5ac3bc6671808a93d3e981b77bb5eb59ebd7f91
HarshParmar346 <harshparmar1234>
2023-03-18 02:19:01 +0530

    Admin Verification activity added

--- app/src/main/java/com/example/hustlers/AdminLoginActivity.java
+++ app/src/main/java/com/example/hustlers/AdminLoginActivity.java
@@ -2,13 +2,39 @@ package com.example.hustlers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.hustlers.admin.AdminDashboard;
 
 public class AdminLoginActivity extends AppCompatActivity {
 
+
+    EditText name;
+    EditText password;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+
+        name = (EditText) findViewById(R.id.editTextTPersonName2);
+        password = (EditText) findViewById(R.id.editTextTPersonName4);
+        Button button = (Button) findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isCorrect()) {
+                    Intent intent = new Intent(AdminLoginActivity.this, AdminDashboard.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+    private boolean isCorrect() {
+        return name.getText().toString().equals("admin") && password.getText().toString().equals("admin");
     }
 }
