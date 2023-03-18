e8ad539f296bb3f2d8f63971e31272286c585afb
HarshParmar346 <harshparmar1234>
2023-03-18 15:33:42 +0530

    Get Token Added

--- app/src/main/java/com/example/hustlers/TokenActivity.java
+++ app/src/main/java/com/example/hustlers/TokenActivity.java
@@ -1,5 +1,6 @@
 package com.example.hustlers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,15 +8,22 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.hustlers.Admin.AdminDashboard;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TokenActivity extends AppCompatActivity {
 
 
-
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     ListView listToken;
-
+    String name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,10 +32,33 @@ public class TokenActivity extends AppCompatActivity {
         listToken = (ListView) findViewById(R.id.listToken);
         GetTokens getTokens = new GetTokens();
         String uid = getIntent().getStringExtra("uid");
-        List<String> list = getTokens.getList(uid);
+
+
+        CollectionReference appRef = db.collection("appointment");
+
+        List<String> list = new ArrayList<>();
+
+        appRef.whereEqualTo("uid", uid).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                for (QueryDocumentSnapshot query : task.getResult()) {
+
+                    String docId = query.getString("docId");
+                    if (docId.equals("001"))
+                        name = "Harsh";
+                    else if (docId.equals("002"))
+                        name = "Ashwim";
+                    else if (docId.equals("003"))
+                        name = "Savio";
+                    else if (docId.equals("004"))
+                        name = "Sarthak";
+
+                    list.add(query.getId() + " " + name + " " + query.getBoolean("isVerified") + " " + query.getLong("token"));
+                }
+            }
+        });
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(TokenActivity.this, android.R.layout.simple_list_item_1, list);
-
         listToken.setAdapter(arrayAdapter);
 
     }
