b4a2f5a57819ca58dbce9e89dfca08df5fd11c41
HarshParmar346 <harshparmar1234>
2023-03-18 05:46:34 +0530

    Admin Verification activity working

--- app/src/main/java/com/example/hustlers/AdminLoginActivity.java
+++ app/src/main/java/com/example/hustlers/AdminLoginActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.hustlers.Admin.AdminDashboard;
+import com.example.hustlers.admin.AdminDashboard;
 
 public class AdminLoginActivity extends AppCompatActivity {
 
